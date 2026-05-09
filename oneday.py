21a1362f03fa65559187421d53ffddd324f06b95
JODev1452 <jerielkonesime1@gmail.com>
2026-05-09 23:05:12 +0000

    Nouvelle modification

--- oneday.py
+++ oneday.py
@@ -2,24 +2,63 @@
 
 # Date : 2026-05-05
 
-def liste():
-    courses = []
-    for i in range(5):
-        item = input("Entrez un article à ajouter à la liste de courses : ")
-        courses.append(item)
-    print("Voici votre liste de courses :")
+print("")
+print("")
+print("---")
+print("")
+print("BIENVENUE DANS LE PROGRAMME DU JOUR : LISTE DE COURSES")
+print("")
+print("---")
+print("")
+print("")
 
-    retire = input("Avez-vous besoin de retirer un article de la liste ? ")
-    if retire.lower() == "oui":
-        item_retire = input("Quel article souhaitez-vous retirer ? ")
-        if item_retire in courses:
-            print(f"Retrait de {item_retire} de la liste.")
-        else:
-            print(f"{item_retire} n'est pas dans la liste.")
+print("")
+print("Aujourd'hui, nous allons créer une liste de courses interactive en Python.")
+print("")
+nom = input("Avant de commencer, quel est votre nom ? ")
+print("")
+print(f"Alors {nom}, commençons !")
 
-return liste 
+print("")
 
-liste_de_courses = liste()
+def creer_liste_courses():
+    liste = []
 
-print(f"Votre liste de courses finale : {liste_de_courses}")
+    for i in range(3):
+        article = input(f"{nom}, Entrez un article à ajouter à votre liste de courses : ")
+        liste.append(article)
 
+    return liste
+
+print("")
+
+liste_courses = creer_liste_courses()
+print(f"{nom}, voici votre liste de courses : {liste_courses}") 
+
+retirer = input(f"{nom}, avez-vous un ou des articles à retirer de votre liste de courses ? (Oui/Non)")
+
+if retirer.lower() == "oui":
+    article_retirer = input(f"{nom}, Quel est le ou l'article à retirer ? : ")
+
+    if article_retirer in liste_courses:
+        liste_courses.remove(article_retirer)
+        print(f"{nom}, vous avez retiré {article_retirer} de votre liste de courses.")
+    else:
+        print("Cet article n'est pas dans votre liste de courses.")
+
+print("")
+
+
+print(f"{nom}, voici votre liste de courses finale : {liste_courses}")
+print("")
+
+print("")
+print("Merci d'avoir utilisé ce programme pour créer votre liste de courses !")
+
+print("")
+print(f"A bientôt, {nom} !")
+print("")
+
+print("")
+print("---")
+print("")
